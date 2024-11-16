7989794f3414167c56fbb03257990ed6d4e03715
Bogdan Tudorache <bogdanmihait10@gmail.com>
2024-11-16 14:52:33 +0200

    add textual substitution primitives

--- lab3/src/Evaluation/Substitution.hs
+++ lab3/src/Evaluation/Substitution.hs
@@ -1,13 +1,19 @@
 module Evaluation.Substitution where
 
 import Syntax.Expression
+-- import Data.List
 import Data.Set
 
 {-|
     Returns the list of free variables in an expression.
 -}
 freeVars :: Expression -> [String]
-freeVars = undefined
+freeVars expr = (toList (freeVarsSet expr))
+
+freeVarsSet :: Expression -> Set String
+freeVarsSet (Var x) = singleton x
+freeVarsSet (Lambda x e) = delete x (freeVarsSet e)
+freeVarsSet (Application e1 e2) = union (freeVarsSet e1) (freeVarsSet e2)
 
 {-|
     Performs the substitution of the free occurrences of a variable within
@@ -17,4 +23,11 @@ subst :: String      -- ^ Variable
       -> Expression  -- ^ New expression
       -> Expression  -- ^ Existing expression
       -> Expression  -- ^ Resulting expression
-subst = undefined
+subst x new curr@(Var y) = if x == y then new else curr
+subst x new curr@(Lambda y e)
+    | x == y = curr
+    | x /= y && not (elem y (freeVars new)) = (Lambda y (subst x new e))
+    | x /= y && (elem y (freeVars new)) && not (elem z freeVarUnion) = (Lambda z (subst x new (subst y (Var z) e)))
+    where z = y ++ "#"
+          freeVarUnion = freeVars (Application new e)
+subst x new curr@(Application e1 e2) = (Application (subst x new e1) (subst x new e2))
